425908a51b988dd07c6b0a967c9a256603d9f441
hugodevweb <100534757+hugodevweb@users.noreply.github.com>
2022-12-14 14:51:31 +0100

    modif btn compte

--- scripts/script.js
+++ scripts/script.js
@@ -30,5 +30,15 @@ function showDiv() {
         });
     });
   });
-  
+
+  $('#btn').click(function() {
+
+    if($('.dropdown').hasClass('is-active')){
+        $('.dropdown').removeClass('is-active') 
+    }
+    else{
+        $('.dropdown').addClass('is-active');
+    }
+   
+});
 
