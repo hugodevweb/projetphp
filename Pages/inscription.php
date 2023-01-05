cd913b74adc89c7a0c9d3cac104e52dac677031c
William <72813547+yuuuks@users.noreply.github.com>
2023-01-05 17:55:45 +0100

    k
    k

--- Pages/inscription.php
+++ Pages/inscription.php
@@ -6,12 +6,16 @@
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
  <link href="../style/main.css" rel="stylesheet">
  </head>
-    <title>Création d'un compte</title>
+    <title>Création de mon compte</title>
 </head>
 <body>
     <div class ="container_inscription">
             <h1>Mon inscription</h1>
+<<<<<<< Updated upstream
             <form class = "form_inscription" action="inscription_action.php" method="POST">
+=======
+            <form class="form_inscription "action="inscription_action.php" method="POST">
+>>>>>>> Stashed changes
 
                 <div class="field">
                 <label class="label">Votre Email</label>
@@ -23,14 +27,22 @@
                 <div class="field">
                   <label class="label">Votre mot de passe</label>
                     <div class="control">
+<<<<<<< Updated upstream
                       <input class="input" name="mdp" type="password" placeholder="Votre mot de passe">
+=======
+                      <input class="input" type="password" placeholder="Votre mot de passe">
+>>>>>>> Stashed changes
                     </div>
                 </div>
 
                 <div class="field">
                   <label class="label">Confirmer votre mot de passe</label>
                     <div class="control">
+<<<<<<< Updated upstream
                       <input class="input" name="confirm_mdp" type="password" placeholder="Votre mot de passe">
+=======
+                      <input class="input" type="password" placeholder="Votre mot de passe">
+>>>>>>> Stashed changes
                     </div>
                 </div>
                   
@@ -39,9 +51,7 @@
                 
                 </form>
         </div>
-        
-        <h2 class="deja_un_compte"> J'ai dejà un <a href="./compte.php"> compte  </a><h2>
 
-    </div>
+        <h2 class="deja_un_compte"> J'ai dejà un <a href="./compte.php"> compte  </a><h2>
 </body>
 </html>
--- Pages/inscription_suite.php
+++ Pages/inscription_suite.php
@@ -6,9 +6,10 @@
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
  <link href="../style/main.css" rel="stylesheet">
  </head>
-    <title>Création d'un compte</title>
+    <title>Création de mon compte</title>
 </head>
 <body>
+<<<<<<< Updated upstream
     <div class ="container_inscription">
         <h1>Finaliser mon inscription</h1>
             <form class = "form_inscription_suite" action="inscription_suite_action.php" method="POST">
@@ -39,8 +40,41 @@
                         <input class="input" name="adresse" type="text" placeholder="Votre mot de passe">
                       </div>
                   </div> 
+=======
+    <div class ="container_inscription_suite">
+            <h1>Finaliser mon inscription</h1>
+            <form class ="form_inscription_suite "action="inscription_action.php" method="POST">
 
+                <div class="field">
+                <label class="label">Votre prénom</label>
+                    <div class="control">
+                      <input class="input" type="text" placeholder="Votre prénom">
+                    </div>
+                </div>
 
+                <div class="field">
+                  <label class="label">Votre nom</label>
+                    <div class="control">
+                      <input class="input" type="text" placeholder="Votre nom">
+                    </div>
+                </div>
+
+                <div class="field">
+                  <label class="label">Votre numéro de téléphone</label>
+                    <div class="control">
+                      <input class="input" type="tel" placeholder="Votre numéro de télephone">
+                    </div>
+                </div>
+>>>>>>> Stashed changes
+
+                <div class="field">
+                  <label class="label">Votre adresse</label>
+                    <div class="control">
+                      <input class="input" type="text" placeholder="Votre adresse">
+                    </div>
+                </div>
+
+<<<<<<< Updated upstream
                   <div class="field is-grouped is-grouped-centered">
                     <div class="field mr-6">
                       <label class="label">Code postal</label>
@@ -61,5 +95,15 @@
 
               </form>
       </div>
+=======
+                            <!--Mettre ville et code postal -->
+
+                </form>
+            </div>   
+            
+            <a href="../Pages/inscription.php">
+        <input type="button" class="bouton_inscription_finale" value="Valider mon inscription">
+    </a>
+>>>>>>> Stashed changes
 </body>
 </html>
