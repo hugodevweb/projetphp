16ad61ea518295d8769e7e6dde2102df4fbcee4a
William <72813547+yuuuks@users.noreply.github.com>
2023-01-05 17:39:31 +0100

    inscription
    inscription

--- Pages/inscription_action.php
+++ Pages/inscription_action.php
@@ -1,20 +1,28 @@
+
 <?php
-
 $bdd = new PDO('mysql:host=localhost;dbname=diggit.me', 'root', '');
-if (isset($_POST['envoi'])) {
-  $mail = htmlspecialchars($_POST['mail']);
-  $mdp = sha1($_POST['mdp']);
+if (isset($_POST['envoi'])) 
+  {
+    if(!empty($_POST['mail']) AND !empty($_POST['mdp']) AND !empty($_POST['confirm_mdp']) )
+    {
+      $email = ($_POST['mail']);
+      $mdp = ($_POST['mdp']);
+      $confirm_mdp = ($_POST['confirm_mdp']);
 
-  $insertClient = $bdd -> prepare('INSERT INTO baseclienttest(mail,mdp) VALUES( ? , ?) ');
-  $insertClient -> execute(array($mail, $mdp ));
-  header("Location: inscription_suite.php");
-
-
-	if (!empty($_POST['mail']) AND !empty($_POST['mdp']) AND !empty($_POST['confirm_mdp']) ){
-
-
-  }else{
-  echo"remplir les champs";
+      if($mdp == $confirm_mdp)
+      {
+        $insert = $bdd -> prepare('INSERT INTO baseclienttest(mail,mdp) VALUES( ? , ? ) ');
+        $insert -> execute(array($email, $mdp ));
+        header('Location: inscription_suite.php');
+      }
+      else
+      {
+        echo 'mdp non identique';
+      }
+    }
+    else
+    {
+      echo 'remplir champr';
+    }
   }
-}
 ?>
